e3498c83db2de50745ab789ae648fa8e3eef1aae
the-jasoney <jasoney.music@gmail.com>
2023-10-26 20:01:31 -0700

    init

--- app/src/main/java/io/github/the_jasoney/trebuchet/AppStates.kt
+++ app/src/main/java/io/github/the_jasoney/trebuchet/AppStates.kt
@@ -1,4 +1,5 @@
 package io.github.the_jasoney.trebuchet
 
 enum class AppStates {
+
 }
--- app/src/main/java/io/github/the_jasoney/trebuchet/components/AppComponent.kt
+++ app/src/main/java/io/github/the_jasoney/trebuchet/components/AppComponent.kt
@@ -1,2 +1,47 @@
+/*
+ * Copyright (C) 2023 Jason Wen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/// A app icon and name that shows up in the home screen
+
 package io.github.the_jasoney.trebuchet.components
 
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.google.accompanist.drawablepainter.DrawablePainter
+
+@Composable
+fun AppComponent(name: String, icon: Drawable) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(5.dp)) {
+        Image(
+            painter = DrawablePainter(icon),
+            contentDescription = null,
+            modifier = Modifier.size(64.dp)
+        )
+        Text(name, fontSize = 15.sp, maxLines = 2, overflow = TextOverflow.Ellipsis)
+    }
+}
